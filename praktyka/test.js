54492234719cd950d18a7ada964c9abb1be4a965
Adam S <adam@sweetanalytics.com>
2023-04-06 21:01:54 +0200

    e

---
+++ praktyka/test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-empty-function */
+class User {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {}
+
+  user() {
+    console.log('test');
+  }
+}
---
+++ praktyka/test2.js
@@ -0,0 +1,3 @@
+console.log(test);
+
+const x = (test) => {};
